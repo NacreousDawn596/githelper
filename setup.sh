d5fed8df1a884a9f9779600463075e10229fa3cc
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-08-23 03:16:46 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,21 @@
+clear
+echo 'downloading files...'
+sleep 1
+sudo apt-get install python3 figlet 
+clear 
+sudo pacman -S python3 figlet 
+clear 
+sudo dnf install python3 figlet 
+clear 
+echo 'configuring files'
+sleep 1
+cd ..
+mkdir ~/.local/share/NacreousDawn596
+mv githelper ~/.local/share/NacreousDawn596
+rm ~/.local/share/NacreousDawn596/githelper/setup.sh
+echo '' >> ~/.bashrc
+echo 'source ~/.local/share/NacreousDawn596/githelper/.help.sh' >> ~/.bashrc
+echo '' >> ~/.bashrc
+clear
+done!
+now you can it by writing githelp after restaring the terminal
